aa9f86a30a1d27527c636ad1bf0d990594af1911
danielnoblenava <danielnoble290@gmail.com>
2026-02-15 21:20:03 -0600

    Add comments and print memory addresses in ej2.c

--- soluciones/ej2.c
+++ soluciones/ej2.c
@@ -1,6 +1,13 @@
 
 #include <stdio.h>
+
+/*
+  La dirección a[5] es mayor que a[4] porque esta más "arriba" en memoria
+*/
+
 int main(){
-    // Resolver ejercicio 2
+    int a[6];
+	printf("%p", &a[4]);
+	printf("%p", &a[5]);
     return 0;
 }

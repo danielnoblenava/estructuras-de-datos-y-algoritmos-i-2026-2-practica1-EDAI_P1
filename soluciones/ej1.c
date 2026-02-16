211715d6745e8fb2c56b121ab573f936f69a3516
danielnoblenava <danielnoble290@gmail.com>
2026-02-15 21:18:47 -0600

    Implement matrix printing in ejercicio 1

--- soluciones/ej1.c
+++ soluciones/ej1.c
@@ -1,6 +1,12 @@
-
 #include <stdio.h>
+
 int main(){
-    // Resolver ejercicio 1
-    return 0;
+	int m[2][4];
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 4; j++)
+			printf("\nm[%d][%d]: %p", i, j, &m[i][j]);
+	printf("\n");
+	return 0;
+
 }
+
